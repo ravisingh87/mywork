b4519933c31cacf4fae58662ab8fb81792c1d5bf
ravisingh87 <ravisingh19870903@gmail.com>
2021-05-21 12:12:22 +0530

    checking test as snapshopt for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  it("renders correctly", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
