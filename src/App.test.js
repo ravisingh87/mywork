467a0187e6b054152e38fae4195199cad965830a
ravisingh87 <ravisingh19870903@gmail.com>
2021-05-21 12:14:44 +0530

    small change in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
   it("renders correctly", () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
-});
+
