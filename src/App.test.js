2460461981f611483a2e985b55a5bb4d1b8441bd
ravisingh87 <ravisingh19870903@gmail.com>
2021-05-21 12:31:36 +0530

    changes in test files

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render} from '@testing-library/react';
-import App from './App';
-
-
-  it("renders correctly", () => {
-    const wrapper = render(<App />);
-    expect(wrapper).toMatchSnapshot();
-  });
-
