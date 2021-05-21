38ccb66264c58b09b3d608cfb8d9d3d04b0567c8
ravisingh87 <ravisingh19870903@gmail.com>
2021-05-21 12:26:03 +0530

    2 changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render} from '@testing-library/react';
 import App from './App';
-import renderer from 'react-test-renderer'
+
 
   it("renders correctly", () => {
-    const wrapper = renderer(<App />);
+    const wrapper = render(<App />);
     expect(wrapper).toMatchSnapshot();
   });
 
